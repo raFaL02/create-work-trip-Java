32b5f519a2287967251c757f8706aab924020745
Rafael de Lima <rafalimabcl@gmail.com>
2024-07-12 22:18:03 -0300

    docs: correct spelling of PARTICIPANT

--- src/main/java/com/planner/planner/participant/ParticipantRepository.java
+++ src/main/java/com/planner/planner/participant/ParticipantRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ParticpantRepository extends JpaRepository<Participant, UUID> {
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
 }
