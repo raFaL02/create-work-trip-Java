ef5e1e6df53e73e62e160e4c5bf0200ad1ba673d
Rafael de Lima <rafalimabcl@gmail.com>
2024-07-13 22:56:01 -0300

    feat: add invite participant

--- src/main/java/com/planner/planner/participant/ParticipantService.java
+++ src/main/java/com/planner/planner/participant/ParticipantService.java
@@ -15,12 +15,24 @@ public class ParticipantService {
 
     public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip) {
         List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
-        
+
         this.repository.saveAll(participants);
 
+        System.out.println(participants.get(0).getId());
+    }
+
+    public ParticipantCreateResponse registerParticipantToEvent(String email, Trip trip) {
+        Participant newParticipant = new Participant(email, trip);
+        this.repository.save(newParticipant);
+
+        return new ParticipantCreateResponse(newParticipant.getId());
     }
 
     public void triggerConfirmationEmailToParticipants(UUID tripId) {
+
+    }
+
+    public void triggerConfirmationEmailToParticipant(String email) {
 
     }
 }
--- src/main/java/com/planner/planner/trip/TripController.java
+++ src/main/java/com/planner/planner/trip/TripController.java
@@ -1,5 +1,8 @@
 package com.planner.planner.trip;
 
+import com.planner.planner.participant.Participant;
+import com.planner.planner.participant.ParticipantCreateResponse;
+import com.planner.planner.participant.ParticipantRequestPayload;
 import com.planner.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -71,4 +76,25 @@ public class TripController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/{id}/invite")
+    public ResponseEntity<ParticipantCreateResponse> inviteParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if ((trip.isPresent())) {
+            Trip rawTrip = trip.get();
+
+            List<String> participantToInvite = new ArrayList<>();
+            participantToInvite.add(payload.email());
+
+            ParticipantCreateResponse participantResponse = this.participantService.registerParticipantToEvent(payload.email(), rawTrip);
+
+            if (rawTrip.getIsConfirmed()) this.participantService.triggerConfirmationEmailToParticipant(payload.email());
+
+            return ResponseEntity.ok(participantResponse);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
